29462768d15237762fc21ff514ddfeb83deb93a7
Miguel Barrios <miguelbh440@gmail.com>
2021-03-17 14:42:01 -0500

    first commit, with working project

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// TODO: add code here
+window.addEventListener("load", () => {
+    getAstronauts();
+    async function getAstronauts(){
+        const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+        const astronauts = await response.json();
+        const container = document.getElementById('container');
+
+        for (let i = 0; i < astronauts.length; i++){
+            container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronauts[i].hoursInSpace}</li>
+                            <li>Active: ${astronauts[i].active}</li>
+                            <li>Skills: ${astronauts[i].skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${astronauts[i].picture}">
+                </div>    
+        `;
+        }
+    }
+});
