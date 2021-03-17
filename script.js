1cc359eabaa0b510332237935068f8a86bd3ce10
Miguel Barrios <miguelbh440@gmail.com>
2021-03-17 15:33:55 -0500

    bonus mission

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ window.addEventListener("load", () => {
     getAstronauts();
     async function getAstronauts(){
         const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
-        const astronauts = await response.json();
+        let astronauts = await response.json();
         const container = document.getElementById('container');
 
+        astronauts = astronauts.sort(function(a, b) {
+            return b.hoursInSpace - a.hoursInSpace;
+            });
+        
+        container.innerHTML = `<p>Number of astronauts: ${astronauts.length+1}</p>`        
         for (let i = 0; i < astronauts.length; i++){
             container.innerHTML += `
                 <div class="astronaut">
@@ -12,13 +17,18 @@ window.addEventListener("load", () => {
                         <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
                         <ul>
                             <li>Hours in space: ${astronauts[i].hoursInSpace}</li>
-                            <li>Active: ${astronauts[i].active}</li>
+                            <li>Active: <span id= "active${i}">${astronauts[i].active}</span></li>
                             <li>Skills: ${astronauts[i].skills}</li>
                         </ul>
                     </div>
                     <img class="avatar" src="${astronauts[i].picture}">
                 </div>    
         `;
+        let active = document.getElementById(`active${i}`); 
+        if (astronauts[i].active == true){
+            active.style.color = 'green';
+        }
+
         }
     }
 });
